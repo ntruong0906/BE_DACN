e78cf5ad51fc95c1fb83b901d1d02543696adeb8
ntruong0906 <132903023+ntruong0906@users.noreply.github.com>
2024-11-19 20:35:28 +0700

    commit

--- src/route/web.js
+++ src/route/web.js
@@ -10,15 +10,15 @@ import handbookController from '../controllers/handbookController'
 let router = express.Router()
 
 let initWebRoutes = (app) => {
-    router.get('/', homeController.getHomePage)
-    router.get('/about', homeController.getAboutPage)
-    router.get('/crud', homeController.getCRUD) //get all user
+    // router.get('/', homeController.getHomePage)
+    // router.get('/about', homeController.getAboutPage)
+    // router.get('/crud', homeController.getCRUD) //get all user
     
-    router.post('/post-crud', homeController.postCRUD) // add user
-    router.get('/get-crud', homeController.displayGetCRUD) // view all user
-    router.get('/edit-crud', homeController.getEditCRUD) //get data need edit
-    router.post('/put-crud', homeController.putCRUD)
-    router.get('/delete-crud', homeController.deleteCRUD)
+    // router.post('/post-crud', homeController.postCRUD) // add user
+    // router.get('/get-crud', homeController.displayGetCRUD) // view all user
+    // router.get('/edit-crud', homeController.getEditCRUD) //get data need edit
+    // router.post('/put-crud', homeController.putCRUD)
+    // router.get('/delete-crud', homeController.deleteCRUD)
 
     //rest-api
     //USER
